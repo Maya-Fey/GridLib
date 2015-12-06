86455edd60604ab7d2f40978249bd0c30891e9b3
MayaFeyIntensifies <mayafey_@mail.be>
2015-12-06 17:19:29 +1300

    Made PositionGrid respect the wrapped parameter

--- src/TestGrid.java
+++ src/TestGrid.java
@@ -4,7 +4,11 @@ public final class TestGrid {
 
 	public static final void main(String[] args)
 	{
-		
+		PositionGrid grid = new PositionGrid(4);
+		grid.setWrapped(true);
+		grid.set(1, 1, 1);
+		grid.moveRel(1, 1, PositionGrid.NORTHEAST, 3);
+		System.out.println(grid.get(0, 0));
 	}
 
 }
