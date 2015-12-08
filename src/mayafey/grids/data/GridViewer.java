8080c6ff0640cef117f97606da30a47638cc4095
Claire <foxcoindev@gmail.com>
2015-12-08 23:24:32 +1300

    ArcticViewer + Tweaks

--- src/mayafey/grids/data/GridViewer.java
+++ src/mayafey/grids/data/GridViewer.java
@@ -3,14 +3,14 @@ package mayafey.grids.data;
 public abstract class GridViewer<Type, Viewer> {
 	
 	protected final PositionGrid grid;
-	protected final GridObjectManager<Type> manager;
+	protected final GridObjectManager<Viewer> manager;
 	
-	public GridViewer(PositionGrid grid, GridObjectManager<Type> manager)
+	public GridViewer(PositionGrid grid, GridObjectManager<Viewer> manager)
 	{
 		this.grid = grid;
 		this.manager = manager;
 	}
 	
-	public abstract void view(GridView<Type> view, Viewer viewer);
+	public abstract void view(GridView<Type> view, int obj);
 
 }
